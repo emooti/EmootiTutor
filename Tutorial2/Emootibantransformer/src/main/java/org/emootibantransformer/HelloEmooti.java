d6e64dad78ee46dc4d4fb2b9f2178cc81d8b1ddb
Uta Kapp <utakapp@googlemail.com>
2016-01-03 20:39:41 +0100

    Tutorial2 initial

---
+++ Tutorial2/Emootibantransformer/src/main/java/org/emootibantransformer/HelloEmooti.java
@@ -0,0 +1,9 @@
+package org.emooti.emootibantransformer;
+
+public class HelloEmooti {
+		public String sayHello(String Name)
+			{
+			return "Hello, how are you feeling today? "+Name;
+			}
+		
+}
