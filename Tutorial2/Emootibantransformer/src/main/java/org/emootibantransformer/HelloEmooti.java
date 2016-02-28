766d9289aa27e8e30346209c0f3f027442095b91
Uta Kapp <utakapp@googlemail.com>
2016-02-28 20:49:18 +0100

    update pom

--- Tutorial2/Emootibantransformer/src/main/java/org/emootibantransformer/HelloEmooti.java
+++
@@ -1,9 +0,0 @@
-package org.emooti.emootibantransformer;
-
-public class HelloEmooti {
-		public String sayHello(String Name)
-			{
-			return "Hello, how are you feeling today? "+Name;
-			}
-		
-}
