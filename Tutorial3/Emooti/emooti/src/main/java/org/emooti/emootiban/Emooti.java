be91a4f6d2b42af01fdd25346d987d1536a8c516
Uta Kapp <utakapp@Utas-MacBook-Air.local>
2017-03-05 19:29:08 +0100

    Tutorial3 refactor Sprint

---
+++ Tutorial3/Emooti/emooti/src/main/java/org/emooti/emootiban/Emooti.java
@@ -0,0 +1,18 @@
+package org.emooti.emootiban;
+
+public class Emooti {
+
+	String emootiID = "00.00.00";
+	
+
+	public String getEmootiID()
+    {
+        return emootiID;
+    }
+ 
+    public void setEmootiID(String emootiID)
+    {
+        this.emootiID = emootiID;
+    }
+    
+    }
